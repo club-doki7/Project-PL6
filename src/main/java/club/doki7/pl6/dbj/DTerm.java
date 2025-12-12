51028106628fa94818186a090fd80bce607d1924
Chuigda Whitegive <chuigda@doki7.club>
2025-12-12 11:39:57 +0800

    Fix fix

--- src/main/java/club/doki7/pl6/dbj/DTerm.java
+++ src/main/java/club/doki7/pl6/dbj/DTerm.java
@@ -2,13 +2,10 @@ package club.doki7.pl6.dbj;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public sealed interface DTerm {
     record Var(@NotNull String name, int index) implements DTerm {
-        @Override
-        public int hashCode() {
-            return Integer.hashCode(index);
-        }
-
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
@@ -17,22 +14,53 @@ public sealed interface DTerm {
         }
 
         @Override
+        public int hashCode() {
+            return Integer.hashCode(index);
+        }
+
+        @Override
         public @NotNull String toString() {
             return name + index;
         }
     }
 
-    record Abs(@NotNull DTerm body) implements DTerm {
+    record Abs(@NotNull String name, @NotNull DTerm body) implements DTerm {
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (!(obj instanceof Abs a)) return false;
+            return this.body.equals(a.body);
+        }
+
+        @Override
+        public int hashCode() {
+            // \x.M == \y.M[y/x], but \x.M != M, so introduce a dummy hash component
+            return Objects.hash("λ", body);
+        }
+
         @Override
         public @NotNull String toString() {
-            return "λ." + body;
+            return "λ" + name + "." + body;
         }
     }
 
     record App(@NotNull DTerm f, @NotNull DTerm arg) implements DTerm {
         @Override
         public @NotNull String toString() {
-            return "(" + f + " " + arg + ")";
+            StringBuilder sb = new StringBuilder();
+            if (!(f instanceof Var)) {
+                sb.append("(").append(f).append(")");
+            } else {
+                sb.append(f);
+            }
+            sb.append(" ");
+            if (!(arg instanceof Var)) {
+                sb.append("(").append(arg).append(")");
+            } else {
+                sb.append(arg);
+            }
+
+            return sb.toString();
         }
     }
 }
