68febd9794ad382bc63a44e867eab7d9fac28ecd
Chuigda Whitegive <chuigda@doki7.club>
2025-12-12 11:44:59 +0800

    Remove the ill-formed `EvalNaive`

--- src/main/java/club/doki7/pl6/Application.java
+++ src/main/java/club/doki7/pl6/Application.java
@@ -1,48 +1,7 @@
 package club.doki7.pl6;
 
-import club.doki7.pl6.naive.EvalNaive;
-import club.doki7.pl6.naive.Term;
-
-import java.util.NoSuchElementException;
-import java.util.Scanner;
-
 public final class Application {
     public static void main(String[] args) {
         System.out.println("PL6 - Lambda Calculus Interpreter");
-        System.out.println("Type 'exit' to quit.");
-
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                System.out.print("> ");
-                String line = scanner.nextLine();
-                if (line.equalsIgnoreCase("exit")) {
-                    break;
-                }
-
-                if (line.isBlank()) {
-                    continue;
-                }
-
-                try {
-                    Term term = Parse.parse(line);
-                    if (term == null) {
-                        continue;
-                    }
-
-                    Term current = term;
-                    while (true) {
-                        Term next = EvalNaive.evalStep(current);
-                        if (next.equals(current)) {
-                            break;
-                        } else {
-                            System.out.println("  = " + next);
-                        }
-                        current = next;
-                    }
-                } catch (Parse.ParseException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        } catch (NoSuchElementException _) {}
     }
 }
